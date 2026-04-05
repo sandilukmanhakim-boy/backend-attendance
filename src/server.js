a0e3a433b5ccf3d8e0a19898e2baf5e21d021d1c
Sandi <sandilukmanhakim@gmail.com>
2026-04-05 20:49:21 +0700

    fix: remove index.js & clean server

--- src/server.js
+++ src/server.js
@@ -5,20 +5,17 @@ const { connectDB } = require("./config/db");
 
 const PORT = process.env.PORT || 3000;
 
-/* =======================
-   START SERVER
-======================= */
 const startServer = async () => {
     try {
-        await connectDB(); // ⬅️ tunggu database siap
-        console.log("PostgreSQL Connected 🚀");
+        await connectDB(); // ⬅️ tunggu DB dulu
+        console.log("✅ PostgreSQL Connected 🚀");
 
         app.listen(PORT, '0.0.0.0', () => {
             console.log(`🚀 Server running on port ${PORT}`);
         });
 
     } catch (error) {
-        console.error("Failed to connect DB:", error);
+        console.error("❌ Failed to connect DB:", error);
         process.exit(1);
     }
 };
